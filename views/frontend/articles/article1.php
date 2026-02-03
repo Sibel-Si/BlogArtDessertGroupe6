f1620bb857528ffc5c015f0fe936375fead2f54e
s-PigaChavigny <sterenn.pigachavigny@gmail.com>
2026-02-03 16:42:24 +0100

    Update article1.php

--- views/frontend/articles/article1.php
+++ views/frontend/articles/article1.php
@@ -42,6 +42,13 @@ $theme = $themes['numThem'] ?? "Thématique par défaut";
 $numMotCles = sql_select("MOTCLEARTICLE", "*", "numArt = $numArt");
 $motscles = sql_select("MOTCLE", "*", "numMotCle = $numMotCles");
 
+$likes = sql_select("LIKEART", "*", "numArt = $numArt");
+$nbLikes = count($likes);
+
+$comments = sql_select("COMMENT", "*", "numArt = $numArt");
+$nbComments = count($comments);
+
+
 ?>
 
 <main class="container">
@@ -49,6 +56,8 @@ $motscles = sql_select("MOTCLE", "*", "numMotCle = $numMotCles");
 		<h1><?php echo $libTitrArt; ?></h1>
 
 		<p class="meta">Publié le <time datetime="<?php echo $dtCreaArt; ?>"></time></p>
+		<p class="meta">Nombre de commentaires : <?php  echo $nbLikes; ?></p>
+        <p class="meta">Nombre de likes : <?php  echo $nbComments; ?></p>
 
 		<p class="chapeau"><?php echo $libChapoArt; ?></p>
 
@@ -79,6 +88,22 @@ $motscles = sql_select("MOTCLE", "*", "numMotCle = $numMotCles");
 			<p> <?php echo $theme; ?></p>
 			<p> Mots Clés: <?php foreach ($motscles as $motcle) {  echo $motcle['libMotCle'] . ' '; } ?></p>
 		</aside>
+
+        <div>
+            <button type="">Like</button>
+        </div>
+        <div>
+            <h3>Commentaires (<?php echo $nbComments; ?>)</h3>
+            <?php foreach ($comments as $comment) { ?>
+                <div>
+                    <p><?php 
+                    $membre = sql_select("MEMBRE", "*", "numMemb = " . $comment['numMemb']);
+                    echo htmlspecialchars($membre['pseudoMemb']); ?> a écrit :</p>
+                    <p><?php echo nl2br(htmlspecialchars($comment['libCom'])); ?></p>
+                </div>
+            <?php } ?>
+            <a href="../comments/commentaire.php" class="btn">Ajputer un commentaire</a>
+        </div>
 
 	</article>
 </main>
