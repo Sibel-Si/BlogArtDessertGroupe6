a919e4985cc274779a37d9539b4f1dc79c9796cc
Sibel-Si <sibel.sever@mmibordeaux.com>
2026-02-03 18:00:50 +0100

    modification footer et début de rédaction rgpd et cookies

--- footer.php
+++ footer.php
@@ -1,9 +1,8 @@
 <!-- Load JS scripts -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+<script href="css/style.css"></script>
 </body>
-</html>
 
-</body>
 <footer>
 	<!-- footer -->
 	<p>BlogArt'26</p>
