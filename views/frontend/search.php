2608c2544c1241358b8d3d9edd23bf6703edaaf2
Sibel-Si <sibel.sever@mmibordeaux.com>
2026-02-04 17:02:10 +0100

    début de code search bar search.php

--- views/frontend/search.php
+++ views/frontend/search.php
@@ -1,24 +1,30 @@
 <?php
 require_once '../../header.php';
-echo ("Recherche");
+
 ?>
-
-<!-- <h1>Recherche Avancée</h1> -->
-
+<!-- <h1>Recherche Avancée</h1>
+<h2>Rechercher par thèmes</h2>
+<h2>Recherche libre</h2> -->
+<br/>
 <h2>Rechercher par mots clés</h2>
+
 <?php 
+
 //SELECT * FROM article WHERE libTitrArt LIKE '%n%';
 
-if(isset($_GET("libMotCle"))){
-    $rechercheTitr = $_GET("libMotCle");
+//on reprend avec un $_post les éléments placés dans la barre de recherche
+$motcle = $_POST("recherche");
+
+//si motcle existe, on la place dans une variable
+if(isset($motcle)){
+    $rechercheTitr = $motcle("recherche");
 }
-$motcle = sql_select("ARTICLE", "*", "LIKE %".$rechercheTitr."%")
+
+//on fait une recherche dans la BDD avec les 
+$recherFinal = sql_select("ARTICLE", "*", "libTitrArt LIKE %".$rechercheTitr."%");
+
 ?>
-
-<!-- <h2>Rechercher par thèmes</h2>
-
-<h2>Recherche libre</h2> -->
-<p></p>
+<?php echo($recherFinal); ?>
 
 <?php
 require_once '../../footer.php';
