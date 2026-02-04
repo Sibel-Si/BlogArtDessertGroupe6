eb6cc5aaad61dd0f6d70c35434ea7b1aba420309
s-PigaChavigny <sterenn.pigachavigny@gmail.com>
2026-02-04 16:39:27 +0100

    fix

--- views/backend/thematiques/create.php
+++ views/backend/thematiques/create.php
@@ -1,15 +1,4 @@
 <?php
-session_start();
-require_once ROOT_URL . '/functions/security.php';
-
-// Define the required level (e.g., 1 for Admin, 2 for Moderator)
-$required_level = 1; 
-
-if (!check_access($required_level)) {
-    // Redirect unauthorized users to login or an error page
-    header("Location: login.php?error=unauthorized");
-    exit(); // Always exit after a header redirect
-}
 
 include '../../../header.php';
 ?>
