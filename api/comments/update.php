a68c315fc61e61fdbea56bc40d9cf91c36946a10
Sibel-Si <sibel.sever@mmibordeaux.com>
2026-02-05 12:56:24 +0100

    control.php (comments) et update.php (api comments)

--- api/comments/update.php
+++ api/comments/update.php
@@ -1,4 +1,12 @@
 <?php
 
 
+require_once $_SERVER['DOCUMENT_ROOT'] . '/config.php';
+require_once '../../functions/ctrlSaisies.php';
 
+$dtCreaCom = ($_GET['dtCreaCom']);
+
+sql_update('COMMENT', 'dtCreaCom', "'$dtCreaCom'");
+
+
+header('Location: ../../views/backend/comments/list.php');
