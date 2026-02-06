bf8daa675d210c8a1ba6ad747abe8f0e88e30bb5
s-PigaChavigny <sterenn.pigachavigny@gmail.com>
2026-02-06 14:14:48 +0100

    fix final final

--- views/backend/articles/delete.php
+++ views/backend/articles/delete.php
@@ -1,3 +1,50 @@
+<?php 
+
+include '../../../header.php';
+check_page_access([1, 2]); 
+
+// Get the article ID from URL parameter
+$numArt = isset($_GET['numArt']) ? (int)$_GET['numArt'] : 0;
+if ($numArt <= 0) { header("Location: list.php"); exit; }
+
+// Validate article ID exists before querying
+if ($numArt <= 0) {
+    header("Location: list.php");
+    exit;
+}
+
+// Fetch the specific article to edit
+$article = sql_select("ARTICLE", "*", "numArt = $numArt");
+$article = !empty($article) ? $article[0] : null;
+if (!$article) { header("Location: list.php"); exit; }
+
+if (!$article) {
+    header("Location: list.php");
+    exit;
+}
+// Check for dependent comments
+$comments = sql_select('COMMENT', 'COUNT(*) as count', "numArt = $numArt");
+$commentCount = $comments[0]['count'] ?? 0;
+
+$themes = sql_select("THEMATIQUE", "*");
+$motscles = sql_select("MOTCLE", "*");
+$motsclesarticles = sql_select("MOTCLEARTICLE", "*", "numArt = $numArt");
+
+// Build array of selected keywords for this article
+$motscleschoisis = array();
+foreach($motsclesarticles as $mca) {
+    $motscleschoisis[] = $mca['numMotCle'];
+}
+$motsclesarticles = sql_select("MOTCLEARTICLE", "numMotCle", "numArt = $numArt");
+$motscleschoisis = array_column($motsclesarticles, 'numMotCle');
+?>
+
+<script src="../../../src/js/articles.js"></script>
+
+<!-- Bootstrap form to edit article -->
+<div class="container">
+    <div class="row">
+
 <div class="col-md-12">
     <form action="<?php echo ROOT_URL . '/api/articles/delete.php' ?>" method="post">
         
